ae39aca492f22cbd731a988d4492eefbd9c77c4b
Goose92 <fabrice_gascoin@yahoo.fr>
2018-10-02 20:00:21 +0200

    Correction suite à découpage en plusieurs fichier

--- ModuleGestionConnexion.py
+++ ModuleGestionConnexion.py
@@ -4,8 +4,10 @@ def InternetOk():
         url = "https://www.google.com"
         urllib.urlopen(url)
         status = "Connected"
+        print("Toto : Connected")
         return True
     except :
         status = "Not connect"
+        print("Toto : Not Connected")
         return False
     # print status
