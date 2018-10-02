5e9149a68e5e1ddf16dd24d3ed821da28e8e7c03
Goose92 <fabrice_gascoin@yahoo.fr>
2018-10-02 20:24:22 +0200

    Correction suite à découpage en plusieurs fichier

--- Main.py
+++ Main.py
@@ -18,6 +18,10 @@ Interval=10  # Temps en seconde entre chaque verification
 # ------------------------------------------------
 # Debut du programme principal
 # ------------------------------------------------
+if len(sys.argv) != 2 :
+    print("Mauvais nombre d'arguments")
+    exit()
+
 if len(sys.argv) == 2 : # Il faut qu'il y ait un argument et un seul (le num pour firebase)
     # A faire : Rajouter le fonctionnement avec un 2eme argument pour Prod (boucle infinie) ou Test (1 repetition) + un 3eme pour l'interval
 	Chaine=sys.argv[1]
