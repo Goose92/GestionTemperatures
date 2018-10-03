db07aaca5aacff21655eb07dd634c2f7bd839efe
Goose92 <fabrice_gascoin@yahoo.fr>
2018-10-03 18:30:16 +0200

    Début mise en place paramètre

--- Main.py
+++ Main.py
@@ -13,7 +13,7 @@ from GestionSonde import read_temp
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-Interval=10  # Temps en seconde entre chaque verification
+
 
 # ------------------------------------------------
 # Debut du programme principal
@@ -36,38 +36,30 @@ if ParamNumSondeFB.isdigit()==False :
 ParamInterval=sys.argv[3]
 if ParamInterval.isdigit()==False :
 	print("Le Troisieme parametre doit etre un nombre indiquant le nombre de minute entre chaque releve")
+	Interval=60*ParamInterval  # Temps en seconde entre chaque verification
 	exit(0)
 
-print("C'est bon, on continue",' / ',ParamTypeLancement,' / ', ParamNumSondeFB,' / ' , ParamInterval)
+print("Lancement de l'application correct (parametres)")
+# On rentre dans la boucle infinie
+firebase = firebase.FirebaseApplication(CompteFirebase,None)
+Boucle=True
 
-if len(sys.argv) == 2 :
-    # A faire : Rajouter le fonctionnement avec un 2eme argument pour Prod (boucle infinie) ou Test (1 repetition) + un 3eme pour l'interval
-    Chaine=sys.argv[2]
-    if Chaine.isdigit() :
-         x = int(Chaine)
-         print(Chaine,"Lancement de l'application correct (un seul argument)")
-         # On rentre dans la boucle infinie
-         firebase = firebase.FirebaseApplication(CompteFirebase,None)
-         Cpt=0
-         while Cpt < 1 :
-             ValTemp=read_temp() # On recupere la temperature actuelle
-             Horodatage=datetime.datetime.now()
-             if InternetOk()==True :   # On regarde si internet est up
-                 # On peut deposer la valeur dans firebase (mais aussi dans la BdD)
-                 ViderBuffer()   # On commence par profiter pour vider le buffer
-                 VariableFirebase="/Temperature/" + Chaine
-                 print(VariableFirebase)
-                 firebase.put(VariableFirebase,'Valeur',ValTemp)
-                 firebase.put(VariableFirebase,'Date',Horodatage)
-                 print("Internet OK",ValTemp,Horodatage)
-             else :
-                 # On ne peut pas deposer dans firebase, on stocke dans le buffer pour plus tard
-                 AjouterItemBuffer([ValTemp,Horodatage])
-                 print("Internet OFF",ValTemp,Horodatage)
-             Cpt=Cpt +1
-             time.sleep(Interval)
-         # Fin de boucle infinie
-    else :
-        print(Chaine,"Echec : Il faut un argument numerique pour indiquer le numero de la sonde")
-else :
-    print("Echec : Il faut un argument numerique et un seul pour indiquer le numero de la sonde")
+while Boucle == True  :
+	ValTemp=read_temp() # On recupere la temperature actuelle
+	Horodatage=datetime.datetime.now()
+	if InternetOk()==True :   # On regarde si internet est up
+	 # On peut deposer la valeur dans firebase (mais aussi dans la BdD)
+	 ViderBuffer()   # On commence par profiter pour vider le buffer
+	 VariableFirebase="/Temperature/" + ParamNumSondeFB
+	 print(VariableFirebase)
+	 firebase.put(VariableFirebase,'Valeur',ValTemp)
+	 firebase.put(VariableFirebase,'Date',Horodatage)
+	 print("Internet OK",ValTemp,Horodatage)
+	else :
+	 # On ne peut pas deposer dans firebase, on stocke dans le buffer pour plus tard
+	 AjouterItemBuffer([ValTemp,Horodatage])
+	 print("Internet OFF",ValTemp,Horodatage)
+	if ParamTypeLancement=="TEST" :  # Si on a lance le programme avec le parametre TEST, on ne fait qu un iteration
+	 Boucle=False
+	time.sleep(Interval)
+# Fin de boucle infinie
